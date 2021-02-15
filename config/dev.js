ce9510c1321856cf76b30b2ed9ea3f8a65035cc6
Ariel Cardoso <arielvix@gmail.com>
2021-02-15 00:45:29 -0300

    Update dependencies

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    googleClientID: '944368609525-q9h3c6b7puau8o05794peqop8t2tkjbn.apps.googleusercontent.com',
-    googleClientSecret: 'R5jqF0UgRe4-JIRdLqm0Y8Ug',
-    mongoURI: 'mongodb+srv://dbuser:MasterKey123@cluster0.wxja5.mongodb.net/emerulyapp?retryWrites=true&w=majority',
-    cookieKey: 'MySecretKey123456!!!',
-    stripePubKey: 'pk_test_51HuoGsEyNHFfKeIDIVwSiFrMrSm5jNa3NAy5GFV9a8dD2SYj0C9jUguS2LuHj4FqQFnF08gkoTEuO1WDFGLfNQOJ00tLMVWUQq',
-    stripeSecretKey: 'sk_test_51HuoGsEyNHFfKeIDJIIVlbS4ZXI3NnJr3b6DKBRKHSHDSLbYe6udzjfikYZW35aXMAW8qP5RebpQVk6cYaEvZ2HU00OhZ8zoIE',
-    redirectDomain: 'http://localhost:5000'
-}
