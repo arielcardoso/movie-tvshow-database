9cdcc0a02da5b9fac12a291f528b52dffaa1d7dd
Ariel Cardoso <arielvix@gmail.com>
2021-02-27 03:12:17 -0300

    Add bootstrap 5

--- client/src/components/TitleItem.jsx
+++ client/src/components/TitleItem.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const TitleItem = (props) => {
     return (
-        <div className="Item" id={"title-"+props.data.id} style={{backgroundImage: 'url(http://image.tmdb.org/t/p/original' + props.data.backdrop_path + ')'}} >
+        <div className="Item" id={"title-"+props.data.id} >
+            <img src={'http://image.tmdb.org/t/p/original' + props.data.backdrop_path} alt={props.data.title} />
             <div className="overlay">
                 <div className="title">{props.data.title}</div>
-                <div className="rating">{props.data.vote_average} / 10</div>
+                <div className="rating">Rating {props.data.vote_average}</div>
+                <div className="icon-button like" title="I like it"><i className="bi bi-heart"></i></div>
+                <div className="icon-button mylist" title="Add to my list"><i className="bi bi-list-check"></i></div>
+                <div className="seasons">2 Temporadas</div>
             </div>
         </div>
     );
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -3,15 +3,27 @@ import React from 'react'
 
 const UserProfile = (props) => {
     return (
-        <div className="user-profile">
-            <img src={props.auth.avatar} />
-            <div className="content">
-                <p>Logged as<br/><strong>{props.auth.name}</strong></p>
-                <hr/>
-                <p><small>{props.auth.email}</small></p>
-                <hr/>
-                <a className="btn" href="/api/auth/logout">Logout</a>
-            </div>
+        <div className="user-profile dropdown" >
+            <a href="#" role="button" id="btnUserProfile" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src={props.auth.avatar} />
+            </a>
+            <ul className="dropdown-menu mt-3" aria-labelledby="btnUserProfile">
+                <li className="dropdown-item" >
+                    Logged as<br/><strong>{props.auth.name}</strong>
+                </li>
+                <li>
+                    <hr className="dropdown-divider"/>
+                </li>
+                <li className="dropdown-item">
+                    <small>{props.auth.email}</small>
+                </li>
+                <li>
+                    <hr className="dropdown-divider"/>
+                </li>
+                <li className="dropdown-item d-grid">
+                    <a className="btn btn-block" href="/api/auth/logout">Logout</a>
+                </li>
+            </ul>
         </div>
     );
 }
